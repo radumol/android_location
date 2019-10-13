bbc35554b4250b965a6526d91ab9640c9903ee16
Radu Molea <radumolea@gmail.com>
2019-10-12 21:32:47 -0400

    gradle dependency, manifest permission, mainactivity code to console log gps coordinates

--- app/src/main/java/com/example/getlocation/MainActivity.java
+++ app/src/main/java/com/example/getlocation/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.getlocation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            Log.d("WTFF", location.toString());
+                        }
+                    }
+                });
     }
 }
